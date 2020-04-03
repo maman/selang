1ef10efb2e9270b93f6ee9fc25bd3ac9ea73221a
Achmad Mahardi <achmad@mahardi.me>
2020-04-03 22:34:09 +0700

    FEAT: add now.json

--- api/hangoutToSlack.ts
+++ api/hangoutToSlack.ts
@@ -1,13 +1,10 @@
-import { NowRequest, NowResponse } from "@now/node";
-import { IHangoutEvent } from "../lib/hangout-types";
+import {NowRequest, NowResponse} from '@now/node';
+import {IHangoutEvent} from '../lib/hangout-types';
 
 export default (req: NowRequest, res: NowResponse) => {
   let responseText;
   const hangoutEvent: IHangoutEvent = req.body;
-  if (
-    hangoutEvent.type === "ADDED_TO_SPACE" &&
-    hangoutEvent.space.type === "ROOM"
-  ) {
+  if (hangoutEvent.type === 'ADDED_TO_SPACE' && hangoutEvent.space.type === 'ROOM') {
     // todo
   }
   /**
@@ -16,5 +13,5 @@ export default (req: NowRequest, res: NowResponse) => {
    * 3. get slack room name from mapping, select by hangout room name.
    * 4. send text to slack room name webhook
    */
-  res.status(200).send({ responseText });
-};
+  res.status(200).send({responseText});
+}
